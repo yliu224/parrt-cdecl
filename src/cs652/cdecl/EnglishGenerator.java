a467fb07c19277af063f646d87d3557116a71b5b
parrt <parrt@cs.usfca.edu>
2017-01-22 12:14:07 -0800

    initial add

---
+++ src/cs652/cdecl/EnglishGenerator.java
@@ -0,0 +1,5 @@
+package cs652.cdecl;
+
+public class EnglishGenerator extends CDeclBaseVisitor<String> {
+	// YOU MUST FILL THIS IN
+}
---
+++ src/cs652/cdecl/Tool.java
@@ -0,0 +1,16 @@
+package cs652.cdecl;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Tool {
+	public static void main(String[] args) {
+		// YOU MUST FILL THIS IN
+	}
+
+	public static String translate(String cdeclText) {
+		// YOU MUST FILL THIS IN
+	}
+}
