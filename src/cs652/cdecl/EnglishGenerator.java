2002f73b649241eb7047b1d97a2fe3c4e437cc7c
dean <yliu224@dons.usfca.edu>
2017-02-08 18:54:26 -0800

    Initial push

--- src/cs652/cdecl/EnglishGenerator.java
+++ src/cs652/cdecl/EnglishGenerator.java
@@ -1,5 +1,7 @@
 package cs652.cdecl;
 
+import g4.CDeclBaseVisitor;
+
 public class EnglishGenerator extends CDeclBaseVisitor<String> {
 	// YOU MUST FILL THIS IN
 }
--- src/cs652/cdecl/Tool.java
+++ src/cs652/cdecl/Tool.java
@@ -1,6 +1,8 @@
 package cs652.cdecl;
 
-import org.antlr.v4.runtime.ANTLRFileStream;
+import g4.CDeclLexer;
+import g4.CDeclParser;
+import org.antlr.v4.gui.Trees;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -8,9 +10,18 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class Tool {
 	public static void main(String[] args) {
 		// YOU MUST FILL THIS IN
+		translate("int *a");
 	}
 
 	public static String translate(String cdeclText) {
-		// YOU MUST FILL THIS IN
+		ANTLRInputStream input = new ANTLRInputStream(cdeclText);
+		CDeclLexer lexer = new CDeclLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		CDeclParser parser = new CDeclParser(tokens);
+		ParseTree tree = parser.declaration();
+		System.out.println(tree.toStringTree(parser));
+		Trees.inspect(tree, parser);
+
+		return "";
 	}
 }
